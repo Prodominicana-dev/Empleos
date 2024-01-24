ee4dee3e9cf19e80d2e86b231089ef168586b178
Fausto-Tineo <faustotineo@prodominicana.gob.do>
2024-01-24 14:27:32 -0400

    Cambios en next.confing.js11

--- next.config.js
+++ next.config.js
@@ -27,18 +27,18 @@ module.exports = nextConfig
 //   },
 // };
 
-// module.exports = {
+module.exports = {
   
-//   distDir: "_next",
-//   generateBuildId: async () => {
-//     if (process.env.BUILD_ID) {
-//       return process.env.BUILD_ID;
-//     } else {
-//       return `${new Date().getTime()}`;
-//     }
-//   },
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },
   
-// }
+}
 
 module.exports = {
   env: {
