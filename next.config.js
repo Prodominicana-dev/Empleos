56add80f07f8123a5d6368a026ceeea3cf4f94de
Fausto-Tineo <faustotineo@prodominicana.gob.do>
2024-01-24 12:29:09 -0400

    Cambios en config y json

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
 
 module.exports = nextConfig
 
+module.exports = {
+  
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },
+  
+}
 
 module.exports = {
   env: {
